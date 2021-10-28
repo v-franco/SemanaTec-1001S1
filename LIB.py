fcae09c868ec27ce07b6be75f56145d48b53888d
Horacio <destroyer_all@hotmail.es>
2021-10-28 17:12:38 -0500

    Add laplaceA && laplaceB methods

--- LIB.py
+++ LIB.py
@@ -1,1 +1,32 @@
 import numpy
+
+
+"""
+Functions laplaceA & laplaceB made by Horacio Lamas
+
+The laplace set of functions are used to find edges on an image. 
+The function "laplaceA" creates and returns a KxK matrix without diagonals.
+The function "laplaceB" creates and returns a KxK matrix with diagonals.
+Kernels from: https://aishack.in/tutorials/image-convolution-examples/
+"""
+def laplaceA(K):
+    M = numpy.zeros((K,K))
+    M[K//2][K//2] = 4
+    M[K//2 + 1][K//2] = -1
+    M[K//2 - 1][K//2] = -1
+    M[K//2][K//2 + 1] = -1
+    M[K//2][K//2 - 1] = -1
+    return M
+
+def laplaceB(K):
+    M = numpy.zeros((K,K))
+    M[K//2][K//2] = 8
+    M[K//2 + 1][K//2] = -1
+    M[K//2 - 1][K//2] = -1
+    M[K//2][K//2 + 1] = -1
+    M[K//2][K//2 - 1] = -1
+    M[K//2 + 1][K//2 + 1] = -1
+    M[K//2 + 1][K//2 - 1] = -1
+    M[K//2 - 1][K//2 + 1] = -1
+    M[K//2 - 1][K//2 - 1] = -1
+    return M
