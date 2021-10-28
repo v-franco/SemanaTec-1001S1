13a6e2388925803c204662f236e3885d5ac40f10
myron57 <a01369053@itesm.mx>
2021-10-28 17:24:32 -0500

    laplacian of gaussian kernel

--- LIB.py
+++ LIB.py
@@ -1,1 +1,16 @@
 import numpy
+
+"""
+Function laplacianOfGaussian made by Myron Bean
+Receives sigma and size of kernel
+Returns laplacian of gaussian kernel
+Formula from: https://homepages.inf.ed.ac.uk/rbf/HIPR2/log.htm
+
+"""
+def laplacianOfGaussian(sigma, K):
+    M = numpy.zeros((K,K))
+    for x in range(0,K):
+        for y in range(0,K):
+            M[x][y] = -(1/(numpy.pi*sigma4)) * (1-((x2+y2)/(2*sigma2))) \
+                        * numpy.exp(-(x2+y2)/(2*sigma**2))
+    return M
